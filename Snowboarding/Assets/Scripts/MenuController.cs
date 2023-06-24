d3fdfcb707c915487cf3c29bfd58259eed8110a1
UnSpoken12 <71053495+kongshijatwit@users.noreply.github.com>
2023-06-24 12:47:06 -0400

    Menu interactions added

--- Snowboarding/Assets/Scripts/MenuController.cs
+++ Snowboarding/Assets/Scripts/MenuController.cs
@@ -6,4 +6,19 @@ public class MenuController : MonoBehaviour
     public void StartBtn(){
         SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
     }
+
+    public void QuitBtn()
+    {
+        Application.Quit();
+    }
+
+    public void RestartBtn()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex - 1);
+    }
+
+    public void MenuBtn()
+    {
+        SceneManager.LoadScene(0);
+    }
 }
